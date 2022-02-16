d2f6f1a4b5771a14b8edaa2dee6835004ccf3cf6
rudmil00 <milanrudanzr@gmail.com>
2022-02-16 21:04:17 +0100

    ubacen falsh samo za login
    Signed-off-by: rudmil00 <milanrudanzr@gmail.com>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -20,3 +20,9 @@ import "controllers";
 //= require turbolinks
 //= require semantic-ui
 //= require_tree .
+$(document).on("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown();
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
+});
